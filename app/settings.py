6f13100117a10d5503b50b8d941db71ae75cb1f4
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-06-01 12:09:53 +0500

    keep secrets to .env file (secret changed)

--- app/settings.py
+++ app/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0^ik4yvve=28@bmkz-!xjb1(^a6_gb!z%edbg95h_*l^m^f+5('
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
+DEBUG = True if DEBUG and DEBUG != '0' else False
 
 ALLOWED_HOSTS = []
 
