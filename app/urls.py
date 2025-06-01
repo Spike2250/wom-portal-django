78a9e0573e070c97caf64673e88e1faac6ec0125
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-06-01 17:22:33 +0500

    add urls

--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from app.users.views import UserLoginView, UserLogoutView
+from app.views import IndexView
+
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='home'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('users/', include('app.users.urls')),
     path('admin/', admin.site.urls),
 ]
