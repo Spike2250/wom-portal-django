688bb199a99f932067cd680384565cdb11493828
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-09-26 20:17:00 +0500

    add Patient model

--- app/patients/models.py
+++ app/patients/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Patient(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False,
+    )
+    fullname = models.CharField(
+        max_length=255, blank=False,
+        verbose_name=_("Patient fullname"),
+    )
+    birthdate = models.DateField(
+        null=False, blank=False,
+        verbose_name=_("Patient birthdate"),
+    )
+    phone = PhoneNumberField(
+        null=False, blank=False, unique=True,
+        verbose_name=_("Patient phone number"),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
