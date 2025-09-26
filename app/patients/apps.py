06b9a45a71d704027564f30db3aaa0b57ea2142a
Shilov Ilia <shilov.spike2250@yandex.ru>
2025-09-26 20:16:23 +0500

    add MAIN_APP_NAME to patients.apps

--- app/patients/apps.py
+++ app/patients/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
+from ..settings import MAIN_APP_NAME
+
 
 class PatientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'patients'
+    name = f'{MAIN_APP_NAME}.patients'
